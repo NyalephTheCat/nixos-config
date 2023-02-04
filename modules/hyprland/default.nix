bec467db1a4d1b1d7749eaccf3275acd28549c3d
nyaleph <contact@nyaleph.com>
2023-02-04 21:47:15 +0100

    fix hyprland

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -6,14 +6,14 @@ let cfg = config.modules.hyprland;
 in {
     options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
     config = mkIf cfg.enable {
-	home.packages = with pkgs; [
-      wofi
-      swaybg 
-      wlsunset
-      wl-clipboard
-      hyprland
-	];
+	  home.packages = with pkgs; [
+        wofi
+        swaybg 
+        wlsunset
+        wl-clipboard
+        hyprland
+	  ];
 
-        home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+      home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
     };
 }
--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -48,6 +48,8 @@
   nix = {
     settings.auto-optimise-store = true;
     settings.allowed-users = [ "chloe" ];
+    settings.substituters = [ "https://hyprland.cachix.org" ];
+    settings.trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     gc = {
       automatic = true;
       dates = "weekly";
