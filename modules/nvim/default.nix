fb52e7ee777ee85daa438ca97db123bad0386ac8
nyaleph <contact@nyaleph.com>
2023-02-06 11:22:37 +0100

    Improved completion in nvim

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -59,10 +59,40 @@ in {
                     plugin = nvim-lspconfig;
                     config = ''
                         lua << EOF
-                        require('lspconfig').rust_analyzer.setup{}
-                        require('lspconfig').sumneko_lua.setup{}
-                        require('lspconfig').rnix.setup{}
-                        require('lspconfig').zk.setup{}
+                        local opts = { noremap = true, silent = true }
+                        vim.keymaps.set('n', '<space>e', vim.diagnostics.open_float, opts)
+                        vim.keymaps.set('n', '[d', vim.diagnostics.goto_prev, opts)
+                        vim.keymaps.set('n', ']d', vim.diagnostics.goto_next, opts)
+                        vim.keymaps.set('n', '<space>q', vim.diagnostics.setloclist, opts)
+
+                        -- Uses an on_attach function to only map the following keys
+                        --  after the language server attaches to the current buffer
+                        local on_attach = function(client, bufnr)
+                          -- Enable completion triggered by <c-x><c-o>
+                          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                          -- Mappings
+                          --  See `:help vim.lsp.*` for documentation on any of the below functions
+                          local bufopts = { noremap = true, silent = true, buffer = bufnr }
+                          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+                          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                          vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+                          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+                          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+                          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+                          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+                          vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, bufopts)
+                          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+                          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+                          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+                          vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+                          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+                        end
+
+                        require('lspconfig').rust_analyzer.setup{ on_attach = on_attach }
+                        require('lspconfig').sumneko_lua.setup{ on_attach = on_attach }
+                        require('lspconfig').rnix.setup{ on_attach = on_attach }
+                        require('lspconfig').zk.setup{ on_attach = on_attach }
                         EOF
                     '';
                 }
@@ -78,7 +108,10 @@ in {
                     }
                     EOF
                     '';
-                }
+                  }
+                  {
+                    plugin = vim-fugitive;
+                  }
             ];
 
             extraConfig = ''
